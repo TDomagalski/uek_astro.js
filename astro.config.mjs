1010505221833fa2a35cfcc34428b53d95768c81
TomDom <tomekwebdev@gmail.com>
2025-08-28 13:48:33 +0200

    fix: sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://uek-astro-js.vercel.app',
+  site: 'https://uek-astro-js.vercel.app/',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
